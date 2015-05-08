16b3248168dd7af53f178ff4c4e69e6f067b24db
Cristian <cristianrobles177@gmail.com>
2015-05-08 14:46:43 -0700

    commit

---
+++ Websites.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>APP HUB</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shcortcut icon" href="IMG/Logo.png">
+        <link type="text/css"  rel="stylesheet" href="CSS/bootstrap.min.css">
+        <link type="text/css"  rel="stylesheet" href="CSS/bootstrap.theme.min.css">
+        <link type="text/css"  rel="stylesheet" href="CSS/Websites.css">
+        <link href="jumbotron.css" rel="stylesheet">
+    </head>
+
+    <body>
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <a  href="index.php" id='APP'><img src="IMG/Logo.png" alt="Logo" height="48" width="80" id='APP' ></a>
+                </div>
+            </div>
+        </nav>
+
+            <div class="jumbotron">
+            <div class="container">
+                <h1>Projects</h1>
+            </div>
+        </div>
+ 
+
+            <hr>
+
+            <footer>
+                <p>&copy; Sainz & Robles Inc. 2015</p>
+            </footer>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+    </body>
+</html>
+
+
